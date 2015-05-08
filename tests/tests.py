308caddc1f560dd1243645b78ae05e43e1057d19
Hans Keeler <hans.keeler@cfpb.gov>
2015-05-08 19:12:45 -0400

    Adds initial /parse tests; skeleton for other tests

--- tests/tests.py
+++ tests/tests.py
@@ -8,6 +8,7 @@ class TestIntegration(object):
         app.app.config['TESTING'] = True
         self.app = app.app.test_client()
 
+
     def test_status(self):
         # Setup
         from datetime import datetime
@@ -28,3 +29,62 @@ class TestIntegration(object):
         assert_equals(data['upSince'], up_since)
         assert_true(data['time'] > time)
         assert_true(data['time'] > data['upSince'])
+
+
+    def test_parse_success(self):
+        # Setup
+        addr_number = '1600'
+        street_name = 'Pennsylvania'
+        street_type = 'Ave'
+        street_direction = 'NW'
+        city = 'Washington'
+        state = 'DC'
+        zip = '20006'
+        address = '1600 Pennsylvania Ave NW Washington DC 20006'
+
+        # Test
+        rv = self.app.get('/parse?address={}'.format(address))
+        data = json.loads(rv.data)
+
+        assert_equals(data['input'], address)
+        
+        parts = data['parts']
+
+        assert_equals(parts['AddressNumber'], addr_number)
+        assert_equals(parts['PlaceName'], city)
+        assert_equals(parts['StateName'], state)
+        assert_equals(parts['StreetName'], street_name)
+        assert_equals(parts['StreetNamePostDirectional'], street_direction)
+        assert_equals(parts['StreetNamePostType'], street_type)
+        assert_equals(parts['ZipCode'], zip)
+
+        from pprint import pprint
+        pprint(data)
+
+
+    def test_parse_fail_invalid_param(self):
+        pass
+
+
+    def test_parse_with_method_tag(self):
+        pass
+
+
+    def test_parse_with_method_parse(self):
+        pass
+
+
+    def test_parse_with_method_invalid(self):
+        pass
+
+
+    def test_parse_with_validate_success(self):
+        pass
+
+
+    def test_parse_with_validate_fail_incomplete(self):
+        pass
+
+
+    def test_parse_with_validate_fail_invalid_parts(self):
+        pass
